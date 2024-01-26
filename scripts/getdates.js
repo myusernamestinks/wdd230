0791afd983ea0dbbdf64ed63ecc305a482e1a494
myusernamestinks <104661176+myusernamestinks@users.noreply.github.com>
2024-01-25 18:23:32 -0600

    added map, hamburger bar and before

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     let lastModifiedDate = new Date(document.lastModified);
     document.getElementById("lastModified").innerText = `Last modified: ${lastModifiedDate.toLocaleString()}`;
 });
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+    document.querySelector('nav ul').classList.toggle('open');
+});
