9ffe21389f7f95473711d39d8d877ae03db51396
myusernamestinks <104661176+myusernamestinks@users.noreply.github.com>
2024-03-05 16:17:36 -0600

    json added but not complete

---
+++ scripts/links.js
@@ -0,0 +1,13 @@
+const baseURL = "https://myusernamestinks.github.io/wdd230/";
+const linksURL = baseURL + "https://myusernamestinks.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+    //displayLinks(data);
+}
+
+
+  
+  getLinks();
