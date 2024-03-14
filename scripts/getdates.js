4e68638e683557cd1f03f16811e68d82a5f97880
myusernamestinks <104661176+myusernamestinks@users.noreply.github.com>
2024-03-14 15:32:39 -0500

    weather added to the homepage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -29,3 +29,37 @@ numVisits++;
 
 localStorage.setItem("numVisits-ls", numVisits);
 
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const latitude = 27.80;
+const longitude = -97.30;
+const apiKey = 'ad014efe3e7c273ec40663e1ad87abb5';
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
