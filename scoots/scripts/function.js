62fa620eebdcd0d5871152a7732592acf783b421
myusernamestinks <104661176+myusernamestinks@users.noreply.github.com>
2024-03-26 22:32:19 -0500

    more changes made to the main layout

---
+++ scoots/scripts/function.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").innerText = currentYear;
+});
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+    document.querySelector('nav ul').classList.toggle('open');
+});
